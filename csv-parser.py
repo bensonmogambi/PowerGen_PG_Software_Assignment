23e9e55726fca59d677af13a78d2486416c1eacc
Benson Mogambi <99333985+bensonmogambi@users.noreply.github.com>
2023-07-10 11:41:14 +0300

    Update on the csv-parser.py
    Code handles the following errors:
    Empty or whitespace rows: It detects and logs an "Empty row" error for any rows in the CSV file that are empty or contain only whitespace.
    
    Missing name: It logs a "Missing name" error if the Name field in a row is empty.
    
    Invalid date of birth: It logs an "Invalid date of birth" error if the DoB field does not match the expected ISO 8601 date format (YYYY-MM-DD) or if the date is not a valid date (e.g., invalid month or day).
    
    Invalid telephone number: It logs an "Invalid telephone number" error if the Phone field does not match the expected format of 12 digits.
    
    Invalid ID number: If the NationalID field is provided, the code can log an "Invalid ID number" error if the is_valid_id_number function returns False (although the function is currently a placeholder and assumes all ID numbers are valid).
    
    Invalid country ID: It logs an "Invalid country ID" error if the CountryID field is missing, not a digit, or not one of the specified values (1, 2, or 3).
    
    Invalid site code: It logs an "Invalid site code" error if the SiteCode field is missing, not a digit, or not a valid site code for the corresponding country.
    
    Duplicate customer record: It detects and logs a "Duplicate customer record" error if a customer with the same combination of name and date of birth already exists.
    
    File not found: It handles the FileNotFoundError exception and prints an appropriate error message if the input file is missing or cannot be found.
    
    Additionally, the code includes generic error handling for csv.Error during parsing. If any error occurs while parsing the CSV file, it logs a "CSV parsing error" with the line number and the exception message.
    
    These error handling mechanisms help improve the robustness of the code by gracefully handling various invalid input scenarios and preventing crashes or unexpected behavior.

--- csv-parser.py
+++ csv-parser.py
@@ -1,4 +1,6 @@
 import csv
+import re
+from datetime import datetime
 
 class Customer:
     def __init__(self, name, date_of_birth, telephone_number, id_number, country_id, site_id):
@@ -17,31 +19,92 @@ def parse_csv(file_path):
     errors = []
     try:
         with open(file_path, 'r') as csvfile:
-            reader = csv.DictReader(csvfile)
-            for line, row in enumerate(reader, start=2):
-                name = row['Name'].strip()
-                date_of_birth = row['DoB'].strip()
-                telephone_number = row['Phone'].strip()
-                id_number = row.get('NationalID', '').strip()
-                country_id = int(row['CountryID'])
-                site_id = int(row['SiteCode'])
+            try:
+                reader = csv.DictReader(csvfile)
+                for line, row in enumerate(reader, start=2):
+                    name = row['Name'].strip()
+                    date_of_birth = row['DoB'].strip()
+                    telephone_number = row['Phone'].strip()
+                    id_number = row.get('NationalID', '').strip()
+                    country_id = row.get('CountryID', '').strip()
+                    site_id = row.get('SiteCode', '').strip()
 
-                if not name or not date_of_birth or not telephone_number:
-                    errors.append({'error': 'Missing required fields', 'line': line})
-                    continue
+                    # Check if the row is empty or contains only whitespace
+                    if not any(row.values()):
+                        errors.append({'error': 'Empty row', 'line': line})
+                        continue
 
-                if country_id not in [1, 2, 3] or site_id not in get_site_ids(country_id):
-                    errors.append({'error': f'Site code {site_id} does not exist in the specified country.', 'line': line})
-                    continue
+                    # Validate name
+                    if not name:
+                        errors.append({'error': 'Missing name', 'line': line})
+                        continue
 
-                customer = Customer(name, date_of_birth, telephone_number, id_number, country_id, site_id)
-                customers.append(customer)
+                    # Validate date of birth
+                    if not is_valid_date(date_of_birth):
+                        errors.append({'error': 'Invalid date of birth', 'line': line})
+                        continue
+
+                    # Validate telephone number
+                    if not is_valid_telephone_number(telephone_number):
+                        errors.append({'error': 'Invalid telephone number', 'line': line})
+                        continue
+
+                    # Validate ID number (optional)
+                    if id_number and not is_valid_id_number(id_number):
+                        errors.append({'error': 'Invalid ID number', 'line': line})
+                        continue
+
+                    # Validate country ID
+                    if not country_id or not country_id.isdigit() or int(country_id) not in [1, 2, 3]:
+                        errors.append({'error': 'Invalid country ID', 'line': line})
+                        continue
+                    country_id = int(country_id)
+
+                    # Validate site ID
+                    if not site_id or not site_id.isdigit() or int(site_id) not in get_site_ids(country_id):
+                        errors.append({'error': 'Invalid site code', 'line': line})
+                        continue
+                    site_id = int(site_id)
+
+                    customer = Customer(name, date_of_birth, telephone_number, id_number, country_id, site_id)
+
+                    # Check for duplicate customer records
+                    if customer in customers:
+                        errors.append({'error': 'Duplicate customer record', 'line': line})
+                        continue
+
+                    customers.append(customer)
+
+            except csv.Error as e:
+                errors.append({'error': 'CSV parsing error', 'line': line, 'exception': str(e)})
 
     except FileNotFoundError:
         print(f"File not found: {file_path}")
         return None, None
 
     return customers, errors
+
+def is_valid_date(date):
+    # Check if the date is in ISO 8601 format (YYYY-MM-DD)
+    pattern = r'^\d{4}-\d{2}-\d{2}$'
+    if not re.match(pattern, date):
+        return False
+
+    # Additional check for valid date components
+    try:
+        datetime.strptime(date, '%Y-%m-%d')
+    except ValueError:
+        return False
+
+    return True
+
+def is_valid_telephone_number(telephone_number):
+    # Check if the telephone number contains only digits and has the expected length
+    pattern = r'^\d{12}$'
+    return bool(re.match(pattern, telephone_number))
+
+def is_valid_id_number(id_number):
+    return True
 
 def get_site_ids(country_id):
     site_ids = {
@@ -51,7 +114,7 @@ def get_site_ids(country_id):
     }
     return site_ids.get(country_id, [])
 
-# Example usage
+# usage
 file_path = 'input.csv'
 customers, errors = parse_csv(file_path)
 
@@ -62,4 +125,7 @@ if customers is not None and errors is not None:
 
     print("\nErrors:")
     for error in errors:
-        print(f"Line {error['line']}: {error['error']}")
+        line = error.get('line', 'unknown')
+        error_msg = error.get('error', 'Unknown error')
+        exception_msg = error.get('exception', '')
+        print(f"Line {line}: {error_msg} - {exception_msg}")
